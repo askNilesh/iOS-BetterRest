06f301d4c1cab4d271dc87385adc21a7e5b08181
AskNilesh <rathodnilsrk@gmail.com>
2023-04-21 22:36:28 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nilesh Rathod on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
